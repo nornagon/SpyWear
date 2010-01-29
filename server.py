e36ffc723cf71e9ce8e56a02c828c3e9cea58913
Jeremy Apthorp <nornagon@nornagon.net>
2010-01-30 01:42:02 +1100

    Add server.py (just echo for now)

---
+++ server.py
@@ -0,0 +1,16 @@
+from twisted.internet import reactor, protocol
+
+class Echo(protocol.Protocol):
+	def dataReceived(self, data):
+		self.transport.write(data)
+
+def main():
+    """This runs the protocol on port 8000"""
+    factory = protocol.ServerFactory()
+    factory.protocol = Echo
+    reactor.listenTCP(8808,factory)
+    reactor.run()
+
+# this only runs if the module was *not* imported
+if __name__ == '__main__':
+    main()
