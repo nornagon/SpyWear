f94e2ccb92dce3a45053d78cc443371bc90dd130
josephg <josephg@gmail.com>
2010-01-30 03:04:42 +1100

    network loop added

--- Awesomest.py
+++ Awesomest.py
@@ -3,6 +3,11 @@
 from pyglet import *
 from model import *
 
+import pygletreactor
+pygletreactor.install()
+
+from twisted.internet import reactor
+
 WIDTH = 1024
 HEIGHT = 768
 
@@ -12,10 +17,18 @@ window = window.Window(width=1024, height=768)
 def on_draw():
 	world.draw(window)
 
+@window.event
+def on_close():
+	print "stopping"
+	app.exit()
+#	reactor.stop()
+	return event.EVENT_HANDLED
+
 def update(dt):
 	world.update(dt)
 
 clock.schedule(update)
 
-app.run()
+reactor.run(call_interval=1/60.)
+#app.run()
 
